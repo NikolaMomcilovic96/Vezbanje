6ff921d7d7e6c125afa94ed8bbc55030c90c319b
NikolaMomcilovic96 <nikolamomca96@gmail.com>
2021-08-21 14:48:37 +0200

    Finished chapter Classes

--- src/main/kotlin/chapter11.kt
+++ src/main/kotlin/chapter11.kt
@@ -1,5 +1,3 @@
-import kotlin.random.Random
-import java.util.*
 import kotlin.math.pow
 
 class Person(var firstName: String, var lastName: String) {
@@ -96,38 +94,214 @@ fun main() {
     println(memberOf(john, groupWithoutJohn))
 
     //Methods and mutability
-    class Grade(
-        val letter: String,
-        val points: Double,
-        val credits: Double
-    )
+//    class Grade(
+//        val letter: String,
+//        val points: Double,
+//        val credits: Double
+//    )
+//
+//    class Student(
+//        val firstName: String,
+//        val lastName: String,
+//        val grades: MutableList<Grade> = mutableListOf(),
+//        var credits: Double = 0.0
+//    ){
+//        val gpa: Double
+//        get(){
+//            var totalPoints = 0.0
+//            grades.forEach{
+//                totalPoints += it.points
+//            }
+//            return totalPoints/credits
+//        }
+//        fun recordGrade(grade: Grade){
+//            grades.add(grade)
+//            credits += grade.credits
+//        }
+//    }
+//
+//    val jane = Student(firstName = "Jane", lastName = "Appleseed")
+//    val history = Grade(letter = "B", points = 9.0, credits = 3.0)
+//    val math = Grade(letter = "A", points = 16.0, credits = 4.0)
+//
+//    jane.recordGrade(history)
+//    jane.recordGrade(math)
+//
+//    println(jane.gpa)
 
     class Student(
-        val firstName: String,
-        val lastName: String,
-        val grades: MutableList<Grade> = mutableListOf(),
-        var credits: Double = 0.0
+        var firstName: String,
+        var lastName: String,
+        var id: Int
     ){
-        val gpa: Double
-        get(){
-            var totalPoints = 0.0
-            grades.forEach{
-                totalPoints += it.points
-            }
-            return totalPoints/credits
+        override fun hashCode(): Int {
+            val prime = 31
+            var result = 1
+
+            result = prime * result + firstName.hashCode()
+            result = prime * result + id
+            result = prime * result + lastName.hashCode()
+
+            return result
         }
-        fun recordGrade(grade: Grade){
-            grades.add(grade)
-            credits += grade.credits
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other)
+                return true
+
+            if (other == null)
+                return false
+
+            if (javaClass != other.javaClass)
+                return false
+
+            val obj = other as Student?
+
+            if (firstName != obj?.firstName)
+                return false
+
+            if (id != obj?.id)
+                return false
+
+            if (lastName != obj?.lastName)
+                return false
+
+            return true
         }
+
+        override fun toString(): String {
+            return "Student (firstName = $firstName, lastName = $lastName, id = $id)"
+        }
+
+        fun copy(
+            firstName: String = this.firstName,
+            lastName: String = this.lastName,
+            id: Int = this.id
+        ) = Student(firstName, lastName, id)
     }
 
-    val jane = Student(firstName = "Jane", lastName = "Appleseed")
-    val history = Grade(letter = "B", points = 9.0, credits = 3.0)
-    val math = Grade(letter = "A", points = 16.0, credits = 4.0)
+    val albert = Student(
+        firstName = "Albert",
+        lastName = "Einstein",
+        id = 1
+    )
+    val richard = Student(
+        firstName = "Richard",
+        lastName = "Feynman",
+        id = 2
+    )
+    val albertCopy = albert.copy()
 
-    jane.recordGrade(history)
-    jane.recordGrade(math)
+    println(albert)
 
-    println(jane.gpa)
+    println(richard)
+
+    println(albert == richard)
+    println(albert == albertCopy)
+    println(albert === albertCopy)
+
+    data class StudentData(
+        var firstName: String,
+        var lastName: String,
+        var id: Int
+    )
+
+    val marie = StudentData("Marie", "Curie", id = 1)
+    val emmy = StudentData("Emmy", "Noether", 2)
+    val marieCopy = marie.copy()
+
+    println(marie)
+    println(emmy)
+    println(marie == emmy)
+    println(marie == marieCopy)
+    println(marie === marieCopy)
+
+
+    val (firstName, lastName, id) = marie
+    println(firstName)
+    println(lastName)
+    println(id)
+
+
+    println("CHALLENGES")
+    println("1. Movie lists")
+
+    val jane = User()
+    val jake = User()
+    val actionList = MovieList(name = "Action")
+
+    jake.addList(actionList)
+    jane.addList(actionList)
+
+    jane.lists["Action"]?.movies?.add("Rambo")
+    jake.lists["Action"]?.movies?.add("Terminator")
+
+    jake.lists["Action"]?.movies?.add("Die Hard")
+
+    jake.lists["Action"]?.print()
+    jane.lists["Action"]?.print()
+
+
+    println("2. T-shirt store")
+
+    val guess = TShirt("L","red",2000.0,"")
+    val nikola = Users("Nikola","nikolamomca96@gmail.com", ShopppingCart())
+    val address = Address("nikola","Svetozara Markovica 9","Belgrade",11130)
+    val korpa = ShopppingCart()
+}
+
+class User(
+    val lists: MutableMap<String, MovieList> = mutableMapOf()
+){
+    fun addList(list: MovieList){
+        lists[list.name] = list
+    }
+
+    fun list(name: String):MovieList?{
+        return lists[name]
+    }
+}
+class MovieList(
+    val name: String,
+    val movies: MutableList<String> = mutableListOf()
+){
+    fun print(){
+        println("Movie list: $name")
+        movies.forEach{
+            println(it)
+        }
+    }
+}
+
+
+data class TShirt(
+    val size: String,
+    val color: String,
+    val price: Double,
+    val image: String
+)
+
+class Users(
+    val name: String,
+    val email: String,
+    val shopppingCart: ShopppingCart
+)
+
+data class Address(
+    val name: String,
+    val street: String,
+    val city: String,
+    val zipCode: Int
+)
+
+class ShopppingCart(
+    var list: MutableList<TShirt> = mutableListOf()
+){
+    fun totalPrice(): Double{
+        var total = 0.0
+        list.forEach{
+            total += it.price
+        }
+        return total
+    }
 }
