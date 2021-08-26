7d2cab3ffdc22d0104e8fc37180553490256ccd0
NikolaMomcilovic96 <nikolamomca96@gmail.com>
2021-08-26 15:27:49 +0200

    Finished chapter 13

--- src/main/kotlin/chapter13.kt
+++ src/main/kotlin/chapter13.kt
@@ -1,5 +1,8 @@
+import kotlin.math.PI
+import kotlin.math.atan
 import kotlin.math.roundToInt
 import kotlin.math.sqrt
+import kotlin.properties.Delegates
 
 fun main(){
     val nikola = Contact(
@@ -51,7 +54,46 @@ fun main(){
     val highestLevel = Level.highestLevel
     println(highestLevel)
 
-    
+    val delegatedLevel1 = DelegatedLevel(id = 1, boss = "Chameleon")
+    val delegatedLevel2 = DelegatedLevel(id = 2, boss = "Squid")
+    println(DelegatedLevel.highestLevel)
+    delegatedLevel2.unlocked = true
+    println(DelegatedLevel.highestLevel)
+
+    val light = LightBulb()
+    light.current = 50
+    var current = light.current
+    light.current = 40
+    current = light.current
+    println("$current A")
+
+    val circle = Circle(5.0)
+    val circumference = circle.circumference
+    println(circumference)
+    //println("PI = ${circle.pi}")
+    println(circle.area)
+    val unitCircle = Circle(1.0)
+    println("Precnik kruga = ${unitCircle.diametar}")
+
+
+    val lamp = Lamp()
+    lamp.bulb = LightBulb()
+    println(lamp.bulb)
+
+
+    println("CHALLENGES")
+    println("1.")
+    val chocolateBanana = IceCream()
+    chocolateBanana.name = "Chocolate Banana"
+    chocolateBanana.ingredients.add("chocolate")
+    chocolateBanana.ingredients.add("banana")
+    println(chocolateBanana.name)
+    println(chocolateBanana.ingredients)
+
+    println("2.")
+    val fiesta = Car("Ford", "metalic", FuelTank())
+    fiesta.fuelTank.level = 0.09
+    println(fiesta.fuelTank.lowFuel)
 }
 
 class Contact(
@@ -109,5 +151,84 @@ class Level(
 ){
     companion object{
         @JvmStatic var highestLevel = 1
+    }
+}
+
+class DelegatedLevel(val id: Int, var boss: String){
+    companion object{
+        var highestLevel = 1
+    }
+    var unlocked: Boolean by Delegates.observable(false){
+        _, old, new ->
+        if (new && id > highestLevel){
+            highestLevel = id
+        }
+        println("$old -> $new")
+    }
+}
+
+class LightBulb{
+    companion object{
+        const val maxCurrent = 40
+    }
+    var current by Delegates.observable(0){
+        _,_,new ->
+        if (new > maxCurrent){
+            println("Current too high, falling back to previous setting.")
+            false
+        }else{
+            println("Current is okay.")
+            true
+        }
+    }
+}
+
+/*
+class Circle(var radius: Double = 0.0){
+    val pi: Double by lazy {
+        ((4.0 * atan(1.0 / 5.0)) - atan(1.0 / 239.0)) * 4.0
+    }
+    val circumference: Double
+    get() = pi * radius * 2
+}*/
+
+class Circle(var radius: Double = 0.0){
+    val area: Double by lazy {
+        PI * radius * radius
+    }
+    val circumference: Double
+    get() = PI * radius * 2
+
+    val diametar: Double
+    get() = 2.0 * radius
+}
+
+class Lamp {
+    lateinit var bulb: LightBulb
+}
+
+//CHALLENGES
+
+//1.
+class IceCream{
+    var name: String = ""
+    val ingredients: ArrayList<String> by lazy {
+        arrayListOf<String>()
+    }
+}
+
+//2.
+class Car(
+    val make: String,
+    val color: String,
+    val fuelTank: FuelTank = FuelTank()
+)
+
+class FuelTank{
+    var lowFuel = true
+
+    var level: Double by Delegates.observable(0.0){
+        _, _, new ->
+        lowFuel = new < 0.1
     }
 }
