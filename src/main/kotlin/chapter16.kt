e7d6a0e3d18d1427b33416fab6adcf4af7e6ee29
NikolaMomcilovic96 <nikolamomca96@gmail.com>
2021-09-15 15:57:58 +0200

    Updated chapter 16

--- src/main/kotlin/chapter16.kt
+++ src/main/kotlin/chapter16.kt
@@ -1,16 +1,72 @@
-enum class DayOfTheWeek{
+import java.util.*
+
+enum class DayOfTheWeek(val isWeekend: Boolean = false) {
     Monday,
     Tuesday,
     Wednesday,
     Thursday,
     Friday,
-    Saturday,
-    Sunday
+    Saturday(true),
+    Sunday(true);
+
+    fun daysUntil(other: DayOfTheWeek): Int {
+        return if (this.ordinal < other.ordinal) {
+            other.ordinal - this.ordinal
+        } else {
+            other.ordinal - this.ordinal + DayOfTheWeek.values().count()
+        }
+    }
+
+    companion object {
+        fun today(): DayOfTheWeek {
+            val calendarDayOfTheWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+
+            var adjustedDay = calendarDayOfTheWeek - 2
+
+            val days = DayOfTheWeek.values()
+
+            if (adjustedDay < 0) {
+                adjustedDay += days.count()
+            }
+
+            val today = days.first { it.ordinal == adjustedDay }
+
+            return today
+        }
+    }
 }
 
-fun main(){
-    for (day in DayOfTheWeek.values()){
-        println("Day ${day.ordinal}: ${day.name}")
+sealed class AcceptedCurrency {
+    abstract val valueInDollars: Float
+    var amount: Float = 0.0f
+
+    class Dollar : AcceptedCurrency() {
+        override val valueInDollars = 1.0f
+    }
+
+    class Euro : AcceptedCurrency() {
+        override val valueInDollars = 1.25f
+    }
+
+    class Crypto : AcceptedCurrency() {
+        override val valueInDollars = 2534.92f
+    }
+
+    val name: String
+        get() = when (this) {
+            is Euro -> "Euro"
+            is Dollar -> "Dollar"
+            is Crypto -> "NerdCoin"
+        }
+
+    fun totalValueInDollars(): Float {
+        return amount * valueInDollars
+    }
+}
+
+fun main() {
+    for (day in DayOfTheWeek.values()) {
+        println("Day ${day.ordinal}: ${day.name}, is weekend: ${day.isWeekend}")
     }
 
     val day = 2
@@ -21,4 +77,26 @@ fun main(){
 
 //    val notADay = DayOfTheWeek.valueOf("Utorak")
 //    println("Not a day: $notADay")
+
+    val today = DayOfTheWeek.today()
+    val isWeekend = "It is ${if (today.isWeekend) "" else "not"} the weekend"
+
+    val secondDay = DayOfTheWeek.Saturday
+    val daysUntil = today.daysUntil(secondDay)
+    println("It is $today. $isWeekend. There are $daysUntil days until $secondDay.")
+
+    when (today) {
+        DayOfTheWeek.Monday -> println("I don't care if $today's blue")
+        DayOfTheWeek.Tuesday -> println("$today's gray")
+        DayOfTheWeek.Wednesday -> println("And $today, too")
+        DayOfTheWeek.Thursday -> println("$today, I don't care 'bout you")
+        DayOfTheWeek.Friday -> println("It's $today, I'm in love")
+        DayOfTheWeek.Saturday -> println("$today, Wait...")
+        DayOfTheWeek.Sunday -> println("$today always comes late")
+        //else -> println("I don't feel like singing")
+    }
+
+    val currency = AcceptedCurrency.Crypto()
+    currency.amount = .27541f
+    println("${currency.amount} of ${currency.name} is ${currency.totalValueInDollars()} in US Dollars!")
 }
